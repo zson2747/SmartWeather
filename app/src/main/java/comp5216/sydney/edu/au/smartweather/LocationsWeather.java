ccdb184939aafbc28ed5ff1fc809065b244b6784
CHEN ZHANG <czha9486@uni.sydney.edu.au>
2019-10-07 18:55:39 +1100

    Locations List created.

--- app/src/main/java/comp5216/sydney/edu/au/smartweather/LocationsWeather.java
+++ app/src/main/java/comp5216/sydney/edu/au/smartweather/LocationsWeather.java
@@ -1,9 +1,32 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package comp5216.sydney.edu.au.smartweather;
 
 public class LocationsWeather {
 
     private String latitude;
     private String longitude;
+    private String json;
+
+    public LocationsWeather(String latitude, String longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        json = null;
+    }
 
     public String getLatitude() {
         return latitude;
@@ -13,11 +36,19 @@ public class LocationsWeather {
         return longitude;
     }
 
+    public String getJson() {
+        return json;
+    }
+
     public void setLatitude(String latitude) {
         this.latitude = latitude;
     }
 
     public void setLongitude(String longitude) {
         this.longitude = longitude;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
     }
 }
--- app/src/main/java/comp5216/sydney/edu/au/smartweather/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/smartweather/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package comp5216.sydney.edu.au.smartweather;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,6 +22,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -16,19 +33,51 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Main Activity for Smart Weather app.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    // Define variables
+    ArrayList<LocationsWeather> arrayOfLocations;
+
+    // Define Constants
+    private final String KEY = "6a700b02d5c29e9cb8e7b41308c994d1";
+
+    /**
+     * Initialize the activity
+     *
+     * @param savedInstanceState Bundle: If the activity is being re-initialized after previously
+     *                           being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). Note:
+     *                           Otherwise it is null. This value may be null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        // Use "activity_main.xml" as the layout
         setContentView(R.layout.activity_main);
+
+        // Construct the data source
+        arrayOfLocations = new ArrayList<LocationsWeather>();
+
+        //TODO: read from database
+
+        //TODO: current location request
+        LocationsWeather current = new LocationsWeather("-33.865143", "151.209900");
+        arrayOfLocations.add(0, current);
+
+        //TODO: save to database
+
         // 开启一个子线程，进行网络操作，等待有返回结果，使用handler通知UI
         new Thread(networkTask).start();
     }
+
 
     Handler handler = new Handler() {
         @Override
@@ -39,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
             Log.i("mylog", "请求结果为-->" + val);
             // TODO
             // UI界面的更新等相关操作
+            TextView tv1 = (TextView) findViewById(R.id.test);
+            tv1.setText(arrayOfLocations.get(0).getJson());
         }
     };
 
@@ -46,14 +97,32 @@ public class MainActivity extends AppCompatActivity {
      * 网络操作相关的子线程
      */
     Runnable networkTask = new Runnable() {
-
         @Override
         public void run() {
+            String result = null;
             // http request.网络请求相关操作
-            try {
-                HttpClient_GET("-33.865143", "151.209900");
-            } catch (Exception e) {
-                e.printStackTrace();
+            for (LocationsWeather location : arrayOfLocations) {
+                try {
+                    result = HttpClient_GET(location.getLatitude(), location.getLongitude());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (result != null) {
+                    location.setJson(result);
+                    //TODO: json处理
+                    /*
+            JSONArray jsonArray=new JSONArray(result);
+            for(int i=0;i<jsonArray.length();i++){
+                HeadNews headnews=new HeadNews();
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                headnews.setTitle(jsonObject.getString("title"));
+                headnews.setContent(jsonObject.getString("content"));
+                headnews.setConut(jsonObject.getString("conut"));
+                headnews.setImage(jsonObject.getString("image"));
+                list.add(headnews);
+            }*/
+                    //TODO: save to database
+                }
             }
             Message msg = new Message();
             Bundle data = new Bundle();
@@ -63,13 +132,21 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    public List<LocationsWeather> HttpClient_GET(String latitude, String longitude) throws Exception {
-
+    /**
+     * 天气API连接
+     *
+     * @param latitude
+     * @param longitude
+     * @return
+     * @throws Exception
+     */
+    public String HttpClient_GET(String latitude, String longitude) throws Exception {
+        String result = null;
         List<LocationsWeather> list = new LinkedList<LocationsWeather>();
         String path = "https://api.darksky.net/forecast/";
         StringBuilder sb = new StringBuilder(path);
         //key
-        sb.append("6a700b02d5c29e9cb8e7b41308c994d1");
+        sb.append(KEY);
         sb.append("/");
         //latitude
         sb.append(latitude);
@@ -91,20 +168,8 @@ public class MainActivity extends AppCompatActivity {
 
         if (status == 200) {
             //读取响应内容
-            String result = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-/*            //json处理
-            JSONArray jsonArray=new JSONArray(result);
-            for(int i=0;i<jsonArray.length();i++){
-                HeadNews headnews=new HeadNews();
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                headnews.setTitle(jsonObject.getString("title"));
-                headnews.setContent(jsonObject.getString("content"));
-                headnews.setConut(jsonObject.getString("conut"));
-                headnews.setImage(jsonObject.getString("image"));
-                list.add(headnews);
-            }*/
-            Log.d("json", result);
+            result = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
         } else Log.d("json", "Error");
-        return list;
+        return result;
     }
 }
