099ab2ef298c78bb4fd8861f9e4ac77212605771
CHEN ZHANG <czha9486@uni.sydney.edu.au>
2019-10-09 23:53:49 +1100

    Current Location Request

--- app/src/main/java/comp5216/sydney/edu/au/smartweather/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/smartweather/MainActivity.java
@@ -16,8 +16,17 @@
 
 package comp5216.sydney.edu.au.smartweather;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -25,9 +34,21 @@ import android.os.Message;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 
 /**
@@ -36,6 +57,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     // Define variables
+    Double currentLatitude;
+    Double currentLongtitude;
     LocationsWeather current;
     ArrayList<LocationsWeather> arrayOfLocations;
     LocationDB db;
@@ -43,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
     int apiRequestCount = 0;
 
     // Define Constants
-    private final String KEY = "6a700b02d5c29e9cb8e7b41308c994d1";
+    private static final String KEY = "6a700b02d5c29e9cb8e7b41308c994d1";
+    private static final int MY_LOCATION_REQUEST_CODE = 1000;
 
     /**
      * Initialize the activity
@@ -59,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Use "activity_main.xml" as the layout
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         // Construct the data source
         arrayOfLocations = new ArrayList<LocationsWeather>();
@@ -68,16 +93,29 @@ public class MainActivity extends AppCompatActivity {
         locationDao = db.locationDao();
         // read from database
         readItemsFromDatabase();
+        // TODO：计时器，半小时内不重复获取location和天气信息
         updateUI();
 
-        //TODO: current location request
-        current = new LocationsWeather("-33.865143", "151.209900");
+        //检测网络，如果连通则获取location信息，更新界面
+        if (isConnected()) {
+            // current location request
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_LOCATION_REQUEST_CODE);
+            } else {
+                getCurrentLocation();
+            }
+        } else {
+            updateUI("NetworkError");
+        }
+
+
         //LocationsWeather current = new LocationsWeather("42.3601", "-71.0589");
 
 
-
         // 开启一个子线程，进行网络操作，等待有返回结果，使用handler通知UI
-        new Thread(networkTask).start();
+        //new Thread(networkTask).start();
     }
 
 
@@ -88,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
             Bundle data = msg.getData();
             String val = data.getString("value");
             Log.i("mylog", "请求结果为-->" + val);
-            updateUI();
+            updateUI(val);
         }
     };
 
@@ -101,47 +139,68 @@ public class MainActivity extends AppCompatActivity {
             Message msg = new Message();
             Bundle data = new Bundle();
             String result = null;
-            // http request.网络请求相关操作
-            try {
-                HTTPClient hc = new HTTPClient();
-                result = hc.HttpClient_GET(current.getLatitude(), current.getLongitude(), KEY);
-            } catch (Exception e) {
-                e.printStackTrace();
-                data.putString("value", "NetworkError");
-            }
-            if (result != null) {
-                apiRequestCount++;
-                JsonHandler jh = new JsonHandler();
-                jh.jsonHandler(current, result);
-                data.putString("value", "200");
-                if (arrayOfLocations.isEmpty()) {
-                    arrayOfLocations.add(current);
+            JsonHandler jh = new JsonHandler();
+
+            if (checkLocationNull(currentLatitude, currentLongtitude)) {
+                current = new LocationsWeather(currentLatitude.toString(),
+                        currentLongtitude.toString());
+                // 尝试更新现在位置的天气
+                try {
+                    // http request.网络请求相关操作
+                    HTTPClient hc = new HTTPClient();
+                    result = hc.HttpClient_GET(current.getLatitude(), current.getLongitude(), KEY);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    data.putString("value", "NetworkError");
+                }
+                // 返回成功
+                if (result != null) {
+                    // api计数
+                    apiRequestCount++;
+                    data.putString("value", "200");
+                    current.setJson(result);
+                    //json处理
+                    jh.jsonHandler(current, result);
                 } else {
+                    data.putString("value", "Error");
+                }
+            } else {
+                current = new LocationsWeather(null, null);
+            }
+            // 首次打开app，只添加现在位置的天气
+            if (arrayOfLocations.isEmpty()) {
+                arrayOfLocations.add(current);
+            } else {
+                if (current.getJson() != null) {
                     arrayOfLocations.set(0, current);
-                    for (int i = 1; i < arrayOfLocations.size(); i++) {
-                        try {
-                            HTTPClient hc = new HTTPClient();
-                            result = hc.HttpClient_GET(arrayOfLocations.get(i).getLatitude(),
-                                    arrayOfLocations.get(i).getLongitude(), KEY);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            data.putString("value", "NetworkError");
-                        }
-                        if (result != null) {
-                            apiRequestCount++;
-                            arrayOfLocations.get(i).setJson(result);
-                            //json处理
-                            jh.jsonHandler(arrayOfLocations.get(i), result);
-                            data.putString("value", "200");
-                        } else {
-                            data.putString("value", "Error");
-                        }
+                }
+            }
+            if (!arrayOfLocations.isEmpty()) {
+                // 除了current, 其他的location获得天气
+                for (int i = 1; i < arrayOfLocations.size(); i++) {
+                    try {
+                        // http request.网络请求相关操作
+                        HTTPClient hc = new HTTPClient();
+                        result = hc.HttpClient_GET(arrayOfLocations.get(i).getLatitude(),
+                                arrayOfLocations.get(i).getLongitude(), KEY);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        data.putString("value", "NetworkError");
+                    }
+                    // 返回成功
+                    if (result != null) {
+                        // api计数
+                        apiRequestCount++;
+                        arrayOfLocations.get(i).setJson(result);
+                        //json处理
+                        jh.jsonHandler(arrayOfLocations.get(i), result);
+                        data.putString("value", "200");
+                    } else {
+                        data.putString("value", "Error");
                     }
                 }
-
-            } else {
-                data.putString("value", "Error");
             }
+
             Log.i("Api Request Count", String.valueOf(apiRequestCount));
             // save to database
             saveItemsToDatabase();
@@ -167,7 +226,8 @@ public class MainActivity extends AppCompatActivity {
                             arrayOfLocations.add(item.getLocationInfo());
                             //TODO: location name
                             Log.i("SQLite read item", "ID: " + item.getLocationID()
-                                    + " Name: " + item.getLocationInfo().getLatitude());
+                                    + ", Latitude: " + item.getLocationInfo().getLatitude() + ", " +
+                                    "Longtitude: " + item.getLocationInfo().getLongitude());
                         }
                     }
                     return null;
@@ -191,22 +251,38 @@ public class MainActivity extends AppCompatActivity {
                 for (LocationsWeather location : arrayOfLocations) {
                     Location item = new Location(location);
                     locationDao.insert(item);
-                    //TODO: location name
-                    if (location.getDailySummary() != null)
-                        Log.i("SQLite saved item", location.getDailySummary());
-                    else
-                        Log.i("SQLite saved item", location.getLatitude());
+                    System.out.println("SQLite saved item: Latitude: " + location.getLatitude() +
+                            ", Longtitude: " + location.getLongitude());
                 }
                 return null;
             }
         }.execute();
     }
 
+
+    /**
+     * UI界面的更新等相关操作，默认连接成功
+     */
+    private void updateUI() {
+        updateUI("200");
+    }
+
     /**
      * UI界面的更新等相关操作
      */
-    private void updateUI() {
+    private void updateUI(String val) {
+        System.out.println("Latitude: " + currentLatitude +
+                ", Longtitude: " + currentLongtitude);
+        if (val == "Error" || val == "NetworkError") {
+            Toast.makeText(this, "Network Connect Error", Toast.LENGTH_LONG).show();
+        } else if (val == null) {
+            Toast.makeText(this, "location permission denied", Toast.LENGTH_LONG).show();
+        }
         // TODO: UI界面的更新等相关操作
+        if (checkLocationNull(currentLatitude, currentLongtitude)) {
+            TextView tv3 = (TextView) findViewById(R.id.test3);
+            tv3.setText(currentLatitude.toString());
+        }
         TextView tv1 = (TextView) findViewById(R.id.test);
         tv1.setMovementMethod(ScrollingMovementMethod.getInstance());
         if (!arrayOfLocations.isEmpty()) {
@@ -215,4 +291,107 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    /**
+     * 检测用户allow或deny location permission
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "location permission granted", Toast.LENGTH_LONG).show();
+                getCurrentLocation();
+            } else {
+                Toast.makeText(this, "location permission denied", Toast.LENGTH_LONG).show();
+                currentLatitude = null;
+                currentLongtitude = null;
+                getCurrentLocation(null);
+            }
+        }
+    }
+
+    /**
+     * current location request
+     */
+    private void getCurrentLocation() {
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+        //TODO: 计时器
+        //为了尽少使用gps，只使用getLastLocation，在半个小时内不获取新的位置
+        //天气app不像map，不需要连续的location获取，也不用setPriority
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<android.location.Location>() {
+                    @Override
+                    public void onSuccess(android.location.Location location) {
+                        // GPS location can be null if GPS is switched off
+                        if (location != null) {
+                            currentLatitude = location.getLatitude();
+                            currentLongtitude = location.getLongitude();
+                        } else {
+                            currentLatitude = null;
+                            currentLongtitude = null;
+                        }
+                        new Thread(networkTask).start();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                        e.printStackTrace();
+                        currentLatitude = null;
+                        currentLongtitude = null;
+                        new Thread(networkTask).start();
+                    }
+                });
+    }
+
+    /**
+     * 没有location permission，直接更新arraylist里剩下的天气
+     *
+     * @param status
+     */
+    private void getCurrentLocation(String status) {
+        //TODO: 计时器
+        new Thread(networkTask).start();
+    }
+
+    /**
+     * 检测current location是否为空
+     *
+     * @param currentLatitude
+     * @param currentLongtitude
+     * @return
+     */
+    private boolean checkLocationNull(Double currentLatitude, Double currentLongtitude) {
+        if (currentLatitude != null && currentLongtitude != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 检查网络连接
+     *
+     * @return 连接成功或失败
+     */
+    public boolean isConnected() {
+        final String command = "ping -c 1 8.8.8.8";
+        boolean isConnected = false;
+        try {
+            isConnected = Runtime.getRuntime().exec(command).waitFor() == 0;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isConnected;
+    }
+
 }
